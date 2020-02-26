95d8077d18883da7e0ad3faf9e97b4d24fab16d9
student <student@DESKTOP-L3ET5DL>
2020-02-26 19:28:46 +0200

    init n demos

---
+++ UnsafeDemo/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnsafeDemo
+{
+    class Data
+    {
+        public string myString = "Data";
+        public Data childData;
+
+        public Data()
+        {
+            Console.WriteLine("new Data");
+        }
+
+        ~Data()
+        {
+            Console.WriteLine("~Data");
+            if (childData != null)
+            {
+                childData.childData = this;
+            }
+        }
+    }
+}
--- UnsafeDemo/Program.cs
+++ UnsafeDemo/Program.cs
@@ -10,6 +10,66 @@ namespace UnsafeDemo
     {
         static void Main(string[] args)
         {
+            //unsafe
+            //{
+            //    // 1
+            //    int[] array = new int[] { 1, 0, 10, 23, 6, -7 };
+            //    fixed (int* p = array) {
+            //        // Console.WriteLine(*p);
+            //        int* currentP = p;
+            //        for (int i = 0; i < array.Length; i++)
+            //        {
+            //            Console.WriteLine(*(currentP++));
+            //        }
+            //    }
+
+            //    // 2
+            //    long a = 15000;
+            //    long b = 1000000000000;
+
+            //    DateTime t1 = DateTime.Now;
+            //    Console.WriteLine(a * b);
+            //    Console.WriteLine(DateTime.Now - t1);
+
+            //    // 1100100
+            //    // a << 3 + a << 2 + a << 0
+            //    // Console.WriteLine(x << 2);
+
+            //    DateTime t2 = DateTime.Now;
+            //    int result = 0;
+            //    while (b != 0)
+            //    {
+            //        if ((b & 1) == 1)
+            //            result += a;
+            //        b >>= 1;
+            //        a <<= 1;
+            //    }
+            //    Console.WriteLine(result);
+            //    Console.WriteLine(DateTime.Now - t2);
+
+            //    a = 15000;
+            //    b = 1000000000000;
+
+            //   DateTime t3 = DateTime.Now;
+            //    Console.WriteLine(a * b);
+            //    Console.WriteLine(DateTime.Now - t3);
+            //}
+
+            // 100
+
+            long startMem = GC.GetTotalMemory(false); // x
+            Data data = new Data();
+            Data data2 = new Data();
+
+            data.childData = data2;
+
+            data = null;
+
+            Console.ReadKey();
+            // x1 - x > 0
+            Console.WriteLine(GC.GetTotalMemory(true) - startMem);
+
+            // 16
         }
     }
 }
